2d3a175bd6ca16fbf800f0e71d2c6eed3dcdab84
fidaatag <fidaa2fidaa@gmail.com>
2023-11-22 23:14:09 +0700

    BMI check - completed

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// get value input
+// get DOM
 const weightInput = document.getElementById('weight');
 const heightInput = document.getElementById('height');
 const BtnCheck = document.getElementById('btn-check');
+const BtnReCheck = document.getElementById('btn-re-check');
+const failNotif = document.querySelector('.fail-notif');
+const BMIres = document.querySelector('.BMI-res');
+const BMIcat = document.querySelector('.BMI-cat');
+const ImgCat = document.getElementById('img-cat');
 
 // action to check!
 BtnCheck.addEventListener('click', (e) => {
@@ -21,10 +26,22 @@ BtnCheck.addEventListener('click', (e) => {
         // select categories if result formula not null
         if (BMIresult !== null) {
             categoriesBMIresult(BMIresult);
+
+            BtnCheck.style.display = 'none';  // Hide the Check button 
+            BtnReCheck.style.display = 'inline-block'; // Display the Recheck button to refresh
+            BtnReCheck.addEventListener('click', refreshPage)
         }
     }
 })
 
+// click red btn to refresh
+const refreshPage = () => location.reload()
+
+
+// Clear text content when input fields are clicked
+const clearFailNotif = () => failNotif.textContent = '';
+weightInput.addEventListener('focus', clearFailNotif);
+heightInput.addEventListener('focus', clearFailNotif);
 
 
 // validasi value input, is ready?
@@ -32,13 +49,13 @@ const checkInput = (w, h) => {
 
     // Checks if both parameters are filled
     if (!w || !h) {
-        console.log('Please provide values for both weight and height.');
+        failNotif.textContent = 'Please provide values for both weight and height.';
         return false;
     }
 
     // Check whether both parameters are numbers
     if (isNaN(w) || isNaN(h)){
-        console.log('Please enter numeric values for weight and height.')
+        failNotif.textContent = 'Please enter numeric values for weight and height.';
         return false;
     }
 
@@ -56,7 +73,16 @@ const formulaBMIcheck = (w, h) => {
     // formula calculation
     const heightInMeter = ((h / 100) ** 2);
     const BMIresult = w / heightInMeter;
-    console.log(BMIresult)
+
+    // Check if BMIresult has decimals
+    const hasDecimals = BMIresult % 1 !== 0;
+
+    // Set the text content based on the condition
+    if (hasDecimals) {
+        BMIres.textContent = BMIresult.toFixed(1); // Display with 2 decimal places
+    } else {
+        BMIres.textContent = BMIresult.toFixed(0); // Display without decimals
+    }
     return BMIresult;
 }
 
@@ -71,13 +97,16 @@ const categoriesBMIresult = (bmi) => {
 
     // Categorize based on BMI value
     if (bmi < 18.5) {
-        console.log('Underweight');
+        BMIcat.textContent = 'under weight'.toUpperCase();
+        ImgCat.src = ('assets/under_weight.svg')
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-         console.log('Normal weight');
-    } else if (bmi >= 25 && bmi <= 29.9) {
-         console.log('Overweight');
+        BMIcat.textContent = 'normal weight'.toUpperCase();
+        ImgCat.src = ('assets/normal_weight.svg')
+     } else if (bmi >= 25 && bmi <= 29.9) {
+        BMIcat.textContent = 'over weight'.toUpperCase();
+        ImgCat.src = ('assets/over_weight.svg')
     } else {
-         console.log('Obesity');
+        BMIcat.textContent = 'obesity'.toUpperCase();
+        ImgCat.src = ('assets/obesity.svg')
     }
 }
-
